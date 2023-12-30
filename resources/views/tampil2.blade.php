198b86c5e0323a7b31d1dc6c4d05f845ba5d99cb
reviwahyuabdillah <reviabdillah29@gmail.com>
2023-12-31 00:10:02 +0700

    first commit

---
+++ resources/views/tampil2.blade.php
@@ -0,0 +1,34 @@
+
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-981/X+9650z00rT7abK413StQ1AqVgRVzpbzoSsmxKp4YTRVH+BabtTE1P161izo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelive.net/npm/popper.15@1.14.7/dist/und/popper.min.js" integrity="sha384-0026T0CpHqd5306nJty5kVphtPhzkjW01c1HTMG3JDZwrn0q4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSavgyd0p3pXB1rRibZUAYOIIy60r06VrjIEaFf/nJGzIxFDsf4x0xIM+B87JRM" crossorigin="anonymous"></script>
+
+
+<center>
+    <h2>Data Mahasiwa</h2>
+    <hr />
+    <table class="table table-dark" style="width:50%; table-align:center;">
+
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>NIM</td>
+            <td>{{$data['nim']}}</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>NAMA</td>
+            <td>{{$data['nama']}}</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>ALAMAT</td>
+            <td>{{$data['alamat']}}</td>
+        </tr>
+    </tbody>
+</table>
+</center>
+<hr />
